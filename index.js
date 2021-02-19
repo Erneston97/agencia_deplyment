30dea41ca7022ebad2c8f6545593299c350b30de
Erneston97 <andres300397@gmail.com>
2021-02-19 16:28:17 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import router from './routes/index.js'
+import db from './config/db.js'
+const app= express();
+
+import dotenv from "dotenv";
+dotenv.config({ path: "./variables.env" });
+
+// conectar la base de datos
+db.authenticate()
+    .then(()=> console.log('Base de datos conectada'))
+    .catch(error => console.log(error));
+
+
+//Definir puerto
+const port=process.env.PORT || 4001;
+
+/** Puerto y host para la app */
+const host = process.env.HOST || '0.0.0.0';
+
+//Habilitar Pug
+app.set('view engine','pug');
+
+//obtener el año actual
+app.use ((req,res,next)=>{
+    const year = new Date();
+
+    res.locals.actualYear=year.getFullYear();
+    res.locals.nombresitio="Agencia de viajes";
+    next();
+});
+// Agregar body parser para leer los datos del formulario 
+app.use(express.urlencoded({extended:true}));
+
+//Definir la carpeta publica 
+app.use(express.static('public'));
+
+//Agregar router
+app.use('/',router);
+
+
+
+
+app.listen(port, host, () => {
+    console.log('El servidor esta funcionando en el puerto ${port} xd')
+})
